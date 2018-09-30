5b853bdb7bf8d343f9ee4f589b3f38d2b6aceb74
tGulevich <t.ananich@gmail.com>
2018-09-30 16:30:11 +0300

    Add Slick slider in Programs

---
+++ script/slider-programs.js
@@ -0,0 +1,50 @@
+$(function(){
+    
+    $('.program').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+    
+    
+    /*
+    var left = document.querySelector('.program__btn-arrow--left');
+    var right = document.querySelector('.program__btn-arrow--right');
+    var programs = document.querySelectorAll('.program__box');
+    var program = document.querySelector('.program__box');
+    var activeElements = document.querySelectorAll('.program__box--active');
+    var hiddenElements = document.querySelector('.program__box--hidden');
+
+    $(".program__btn-arrow--right").on('click', function() {
+
+        console.log($(".program__box--active"));
+        
+        if($(".program__box--active").next(".program__box--hidden").hasClass("program__box--hidden")) {
+            
+            $(".program__box--active").first().toggleClass("program__box--active program__box--hidden");       
+            $(".program__box--active").next(".program__box--hidden").toggleClass("program__box--hidden program__box--active");
+
+        } else {
+            $(".program__box--active").first().toggleClass("program__box--active program__box--hidden");
+            $(".program__box--hidden").first().appendTo($(".program__box__list"));
+            $(".program__box--active").next(".program__box--hidden").toggleClass("program__box--hidden program__box--active");
+            
+        }
+        
+    });
+
+    left.addEventListener('click', function() {
+
+        if($(".program__box--active").prev(".program__box--active").hasClass("program__box--hidden")) {
+            $(".program__box--active").last().toggleClass("program__box--active program__box--hidden");
+            $(".program__box--active").prev(".program__box--hidden").toggleClass("program__box--hidden program__box--active");
+            
+        } else {
+            $(".program__box--active").last().toggleClass("program__box--active program__box--hidden");
+            $(".program__box--hidden").last().prependTo($(".program__box__list"));
+            $(".program__box--active").prev(".program__box--hidden").toggleClass("program__box--hidden program__box--active");
+        }
+
+    });
+    */
+});
